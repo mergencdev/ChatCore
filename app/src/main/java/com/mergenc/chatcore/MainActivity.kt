dfcdb77c04511ceb7a81e288eba07e55b849153f
Mehmet Emin Ergenç <mehmet.ergenc@appcent.mobi>
2022-01-12 17:10:31 +0300

    -get tabs for Main screen -bugfix for themes.xml

--- app/src/main/java/com/mergenc/chatcore/MainActivity.kt
+++ app/src/main/java/com/mergenc/chatcore/MainActivity.kt
@@ -2,12 +2,41 @@ package com.mergenc.chatcore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
+import com.mergenc.chatcore.Adapter.ViewPagerAdapter
+import com.mergenc.chatcore.Fragment.ChatFragment
+import com.mergenc.chatcore.Fragment.CoreFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var pager: ViewPager
+    private lateinit var tab: TabLayout
+    private lateinit var bar: Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //https://www.geeksforgeeks.org/android-material-tabs-in-kotlin/;
+        pager = findViewById(R.id.viewPager)
+        tab = findViewById(R.id.tabs)
+        bar = findViewById(R.id.toolbar)
 
+        // To make our toolbar show the application
+        // we need to give it to the ActionBar
+        setSupportActionBar(bar)
+
+        val adapter = ViewPagerAdapter(supportFragmentManager)
+
+        // add fragment to the list
+        adapter.addFragment(ChatFragment(), "CHAT")
+        adapter.addFragment(CoreFragment(), "CORE")
+
+        // Adding the Adapter to the ViewPager
+        pager.adapter = adapter
+
+        // bind the viewPager with the TabLayout.
+        tab.setupWithViewPager(pager)
     }
 }
